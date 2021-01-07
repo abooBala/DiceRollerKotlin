c53184690c4769b50803d2414846ea884303283d
abooBala <abubakar.bala@whogohost.com>
2021-01-08 00:02:15 +0100

    Added roll functionality to display the dice rolled and reset images back to 200X250 dp for better display

--- app/src/main/java/com/google/codelabs/dicerollerkotlin/MainActivity.kt
+++ app/src/main/java/com/google/codelabs/dicerollerkotlin/MainActivity.kt
@@ -3,7 +3,7 @@ package com.google.codelabs.dicerollerkotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 
@@ -16,53 +16,27 @@ class MainActivity : AppCompatActivity() {
         val rollButton : Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countUp : Button = findViewById(R.id.count_up)
-        countUp.setOnClickListener { countUp() }
-
-        val reset : Button = findViewById(R.id.reset_button)
-        reset.setOnClickListener { resetDice() }
-
     }
 
     private fun rollDice() {
 
-        val resulltText: TextView = findViewById(R.id.result_text)
-
         val randomInt = (1..6).random()
 
 
-//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
 
-        resulltText.text = randomInt.toString()
+        val drawableResource = when (randomInt){
 
-    }
-
-    private fun countUp() {
-
-        val resulltText: TextView = findViewById(R.id.result_text)
-
-//        Check to see if result text contains "Hello World"
-        if (resulltText.text == "Hello World!") {
-            resulltText.text = "1"
-        } else {
-
-            // else increase the value
-            var resultNumber = resulltText.text.toString().toInt()
-            // only increase the value by 1 if the number is less than 6
-            if ( resultNumber < 6) {
-                resultNumber++
-                resulltText.text = resultNumber.toString()
-            }
-
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-    }
 
-    private fun resetDice() {
-
-        val resulltText: TextView = findViewById(R.id.result_text)
-        resulltText.text = "0"
-
-
+        diceImage.setImageResource(drawableResource)
 
     }
+
 }
